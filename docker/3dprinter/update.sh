88cb09b6cc454b3810467bd884fb025ecd97a9c8
Dimitris Zervas <dzervas@dzervas.gr>
2020-11-18 22:11:21 +0200

    Remove 3d printer

--- docker/3dprinter/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker-compose build --no-cache
-docker-compose up -d
-docker-compose exec octoprint bash -c "supervisorctl stop klipper && cd /klipper && make menuconfig && make && make flash FLASH_DEVICE=/dev/ttyUSB0 && supervisorctl start klipper"
