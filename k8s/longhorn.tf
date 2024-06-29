8e4942963128672ca6ee1855a89126775a787d5c
Dimitris Zervas <dzervas@dzervas.gr>
2024-06-29 21:23:00 +0300

    Fix longhorn
    Signed-off-by: Dimitris Zervas <dzervas@dzervas.gr>

--- k8s/longhorn.tf
+++ k8s/longhorn.tf
@@ -11,3 +11,32 @@ resource "kubernetes_namespace" "longhorn-system" {
     }
   }
 }
+
+resource "helm_release" "longhorn" {
+  name       = "longhorn"
+  namespace  = kubernetes_namespace.longhorn-system.metadata.0.name
+  repository = "https://charts.longhorn.io"
+  chart      = "longhorn"
+  version    = "1.6.2"
+  values = [yamlencode({
+    persistence = {
+      defaultClass             = true
+      defaultDataLocality      = "best-effort"
+      defaultClassReplicaCount = 2
+    }
+    ingress = {
+      enabled          = true
+      ingressClassName = "nginx"
+      annotations = {
+        "cert-manager.io/cluster-issuer"                     = "letsencrypt"
+        "nginx.ingress.kubernetes.io/ssl-redirect"           = true
+        "nginx.ingress.kubernetes.io/auth-tls-verify-client" = "on"
+        "nginx.ingress.kubernetes.io/auth-tls-secret"        = "cert-manager/client-ca-certificate"
+        "nginx.ingress.kubernetes.io/auth-tls-verify-depth"  = 1
+      }
+      host      = "storage.${var.domain}"
+      tls       = true
+      tlsSecret = "storage-${replace(var.domain, ".", "-")}-cert"
+    }
+  })]
+}
