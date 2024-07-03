b7ab7efa27453964ed385c0c47bb02e7b3f4e5be
Dimitris Zervas <dzervas@dzervas.gr>
2024-07-04 00:24:15 +0300

    Add modem scraper
    Signed-off-by: Dimitris Zervas <dzervas@dzervas.gr>

--- k8s/prometheus.tf
+++ k8s/prometheus.tf
@@ -20,3 +20,75 @@ resource "helm_release" "prometheus" {
     }
   })]
 }
+
+resource "kubernetes_endpoints_v1" "modem_node_exporter" {
+  metadata {
+    name      = "modem-node-exporter"
+    namespace = "prometheus"
+    labels = {
+      app        = "modem-node-exporter"
+      managed_by = "terraform"
+    }
+  }
+
+  subset {
+    address {
+      ip = "10.11.12.1"
+    }
+    port {
+      name     = "metrics"
+      port     = 9100
+      protocol = "TCP"
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "modem_node_exporter" {
+  metadata {
+    name      = "modem-node-exporter"
+    namespace = "prometheus"
+    labels = {
+      app        = "modem-node-exporter"
+      managed_by = "terraform"
+    }
+  }
+
+  spec {
+    type = "ClusterIP"
+    port {
+      name        = "metrics"
+      protocol    = "TCP"
+      port        = 9100
+      target_port = 9100
+    }
+  }
+}
+
+resource "kubernetes_manifest" "prometheus_modem_monitor" {
+  manifest = {
+    apiVersion = "monitoring.coreos.com/v1"
+    kind       = "ServiceMonitor"
+    metadata = {
+      name      = "modem-monitor"
+      namespace = "prometheus"
+      labels = {
+        managed_by = "terraform"
+      }
+    }
+    spec = {
+      selector = {
+        matchLabels = {
+          app        = "modem-node-exporter"
+          managed_by = "terraform"
+        }
+      }
+      endpoints = [
+        {
+          port     = 9100
+          interval = "30s"
+          path     = "/metrics"
+        }
+      ]
+    }
+  }
+}
