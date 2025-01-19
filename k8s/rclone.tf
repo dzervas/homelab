dde9cf4319b16e08851f91ce5686d7043879855d
Dimitris Zervas <dzervas@dzervas.gr>
2025-01-20 00:11:51 +0200

    Use crypt with s3
    Signed-off-by: Dimitris Zervas <dzervas@dzervas.gr>

--- k8s/rclone.tf
+++ k8s/rclone.tf
@@ -26,12 +26,16 @@ module "rclone" {
     <<EOF
     mkdir -p /config/rclone && \
     cp /secret/rclone.conf /config/rclone/rclone.conf && \
-    rclone serve s3 remote:/rclone/s3 \
+    rclone serve s3 remote: \
     --vfs-cache-mode full \
     --addr 0.0.0.0:80 \
     --auth-key "${random_password.rclone_access_key.result}${random_password.rclone_secret_key.result}"
     EOF
   ]
+}
+
+data "external" "crypt_password" {
+  program = ["bash", "-c", "echo {\\\"password\\\": \\\"$(rclone obscure ${local.op_secrets.rclone.crypt_password})\\\", \\\"password2\\\": \\\"$(rclone obscure ${local.op_secrets.rclone.crypt_salt})\\\"}"]
 }
 
 resource "kubernetes_secret_v1" "rclone" {
@@ -42,7 +46,7 @@ resource "kubernetes_secret_v1" "rclone" {
 
   data = {
     "rclone.conf" = <<EOF
-    [remote]
+    [remote_raw]
     type                 = onedrive
     client_id            = ${local.op_secrets.rclone.client_id}
     client_secret        = ${local.op_secrets.rclone.client_secret}
@@ -56,6 +60,13 @@ resource "kubernetes_secret_v1" "rclone" {
     token_url            = https://login.microsoftonline.com/${local.op_secrets.rclone.tenancy_id}/oauth2/v2.0/token
     token                = ${local.op_secrets.rclone.token}
     drive_id             = ${local.op_secrets.rclone.drive_id}
+
+    [remote]
+    type = crypt
+    remote = remote_raw:rclone/s3
+    filename_encoding = base32768
+    password = ${data.external.crypt_password.result.password}
+    password2 = ${data.external.crypt_password.result.password2}
     EOF
   }
 }
