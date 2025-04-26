fd8b797a70100b91a11f75120b0e42895a1ddebe
Dimitris Zervas <dzervas@dzervas.gr>
2025-04-26 04:28:08 +0300

    Enable auth-url
    Signed-off-by: Dimitris Zervas <dzervas@dzervas.gr>

--- k8s/nextcloud.tf
+++ k8s/nextcloud.tf
@@ -15,9 +15,9 @@ locals {
 module "nextcloud_ingress" {
   source = "./ingress-block"
 
-  namespace    = "nextcloud"
-  fqdn         = "files.${var.domain}"
-  mtls_enabled = true
+  namespace = "nextcloud"
+  fqdn      = "files.${var.domain}"
+  # mtls_enabled = true
   additional_annotations = {
     "cert-manager.io/cluster-issuer"              = "letsencrypt"
     "magicentry.rs/name"                          = "NextCloud"
@@ -26,8 +26,8 @@ module "nextcloud_ingress" {
     "magicentry.rs/manage-ingress-nginx"          = "true"
     "nginx.ingress.kubernetes.io/ssl-redirect"    = "true"
     "nginx.ingress.kubernetes.io/proxy-body-size" = "10g"
-    # "nginx.ingress.kubernetes.io/auth-url"        = "http://magicentry.auth.svc.cluster.local:8080/auth-url/status"
-    # "nginx.ingress.kubernetes.io/auth-signin"     = "https://auth.dzerv.art/login"
+    "nginx.ingress.kubernetes.io/auth-url"        = "http://magicentry.auth.svc.cluster.local:8080/auth-url/status"
+    "nginx.ingress.kubernetes.io/auth-signin"     = "https://auth.dzerv.art/login"
   }
 }
 
