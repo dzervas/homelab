bc59c35227e51b07fb97839fb077e124a097c517
Dimitris Zervas <dzervas@dzervas.gr>
2025-07-17 18:33:05 +0300

    Remove nocodb rclone

--- k8s/nocodb.tf
+++ k8s/nocodb.tf
@@ -1,16 +1,11 @@
 module "nocodb" {
   source = "./docker-service"
 
-  type                    = "statefulset"
-  name                    = "nocodb"
-  fqdn                    = "db.${var.domain}"
-  create_namespace        = true
-  ingress_enabled         = true
-  auth                    = "mtls"
-  enable_security_context = false
-  rclone_access           = true
-  #   vpn_bypass_auth  = true
-  #   vpn_cidrs        = var.vpn_cidrs
+  type = "statefulset"
+  name = "nocodb"
+  fqdn = "db.${var.domain}"
+  auth = "mtls"
+
   image         = "nocodb/nocodb:latest"
   port          = 8080
   retain_pvs    = true
@@ -32,49 +27,6 @@ module "nocodb" {
   env = {
     TZ                     = var.timezone
     NC_PUBLIC_URL          = "https://db.${var.domain}"
-    NC_S3_ENDPOINT         = "rclone.rclone.svc.cluster.local"
-    NC_S3_BUCKET_NAME      = "nocodb"
-    NC_S3_REGION           = "auto"
-    NC_S3_FORCE_PATH_STYLE = true
-    # TODO: Backup to S3
-    # LITESTREAM_S3_ENDPOINT      = "rclone.rclone.svc.cluster.local:8080"
-    # LITESTREAM_S3_BUCKET_NAME   = "nocodb"
-    # LITESTREAM_S3_REGION        = "auto"
-    # LITESTREAM_S3_PATH          = "backups"
-    # LITESTREAM_S3_ACCESS_KEY    = random_password.rclone_access_key.result
-    # LITESTREAM_S3_ACCESS_SECRET = random_password.rclone_secret_key.result
-    # LITESTREAM_RETENTION        = "720" # 30 days
-    # LITESTREAM_AGE_PUBLIC_KEY   = data.onepassword_item.nocodb_age_key.public_key
-    # LITESTREAM_AGE_SECRET_KEY   = data.onepassword_item.nocodb_age_key.private_key
-  }
-
-  env_secrets = {
-    NC_S3_ACCESS_KEY = {
-      secret = "rclone-s3-op"
-      key    = "access-id"
-    }
-    NC_S3_ACCESS_SECRET = {
-      secret = "rclone-s3-op"
-      key    = "secret-key"
-    }
-  }
-}
-
-resource "kubernetes_manifest" "nocodb_s3_op" {
-  manifest = {
-    apiVersion = "external-secrets.io/v1"
-    kind       = "ExternalSecret"
-    metadata = {
-      name      = "rclone-s3-op"
-      namespace = module.nocodb.namespace
-    }
-    spec = {
-      secretStoreRef = {
-        name = "1password"
-        kind = "ClusterSecretStore"
-      }
-      dataFrom = [ { extract = { key = "rclone-s3" } } ]
-    }
   }
 }
 
