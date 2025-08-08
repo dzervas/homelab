85ab886e65ef00d10cd2419266eebab0187e2882
Dimitris Zervas <dzervas@dzervas.gr>
2025-08-08 04:27:10 +0300

    Add headplane - didn't work

--- k8s/headscale.tf
+++ k8s/headscale.tf
@@ -7,15 +7,17 @@ module "headscale" {
   auth  = "none"
   image = "ghcr.io/juanfont/headscale"
   port  = 8080
+  args  = ["serve"]
+  # TODO: Don't terminate the SSL at the ingress
 
   metrics_port = 9090
 
-  liveness_http_path  = "/"
+  # liveness_http_path = "/"
 
   pvs = {
     "/var/lib/headscale" = {
       name = "db"
-      size = "1Gi"
+      size = "512Mi"
     }
   }
 
@@ -33,18 +35,56 @@ resource "kubernetes_config_map_v1" "headscale_config" {
   data = {
     "config.yaml" = yamlencode({
       disable_check_updates = true
-      server_url = "https://vpn.${var.domain}"
-      listen_addr = "0.0.0.0:8080"
-      metrics_listen_addr = "0.0.0.0:8080"
+      server_url            = "https://vpn.${var.domain}"
+      listen_addr           = "0.0.0.0:8080"
+      metrics_listen_addr   = "0.0.0.0:9090"
+
+      # grpc_listen_addr    = "0.0.0.0:50443"
+      # grpc_allow_insecure = true
+
+      noise = {
+        private_key_path = "/var/lib/headscale/noise_private.key"
+      }
+
+      database = {
+        type = "sqlite3"
+        sqlite = {
+          path               = "/var/lib/headscale/db.sqlite"
+          write_ahead_log    = true
+          wal_autocheckpoint = 1000
+        }
+      }
 
       dns = {
-        base_domain = "ts.${var.domain}"
-        # extra_records_path = "/etc/headscale/dns.json"
+        base_domain        = "ts.${var.domain}"
+        override_local_dns = false
+        extra_records_path = "/etc/headscale/dns.json"
       }
 
       prefixes = {
-        v4 = "100.100.50.0/24"
+        allocation = "sequential"
+        v4         = "100.100.50.0/24"
       }
+
+      derp = {
+        server = {
+          enabled        = false
+          region_id      = 999
+          region_code    = "homelab"
+          region_name    = "HomeLab"
+          verify_clients = true
+        }
+
+        urls                = ["https://controlplane.tailscale.com/derpmap/default"]
+        auto_update_enabled = true
+        update_frequency    = "24h"
+      }
+
+      ephemeral_node_inactivity_timeout = "30m"
+
+      unix_socket            = "/tmp/headscale.sock"
+      unix_socket_permission = "0700"
     })
+    "dns.json" = jsonencode([])
   }
 }
