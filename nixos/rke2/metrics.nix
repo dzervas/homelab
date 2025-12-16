25d8686b7681dc3c28f98a1633b4f5b9368d63ff
Dimitris Zervas <dzervas@dzervas.gr>
2025-12-16 03:25:47 +0200

    Fix node exporter firewall issue

--- nixos/rke2/metrics.nix
+++ nixos/rke2/metrics.nix
@@ -1,12 +1,24 @@
 {
+	config,
   hostIndex,
+  node-vpn-iface,
   node-vpn-prefix,
   ...
 }: let
   listenAddress = "${node-vpn-prefix}.${hostIndex}";
 in {
   services.prometheus.exporters = {
-    node = { enable = true; inherit listenAddress; };
-    smartctl = { enable = true; inherit listenAddress; };
+    node = {
+	    inherit listenAddress;
+	    enable = true;
+			openFirewall = true;
+			firewallRules = ''iifname ${node-vpn-iface} tcp dport ${toString config.services.prometheus.exporters.node.port} counter accept'';
+    };
+    smartctl = {
+	    inherit listenAddress;
+	    enable = true;
+			openFirewall = true;
+			firewallRules = ''iifname ${node-vpn-iface} tcp dport ${toString config.services.prometheus.exporters.smartctl.port} counter accept'';
+    };
   };
 }
