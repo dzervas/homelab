3790f106aadd23ba4130c1f4171db05ce7d72f5a
Dimitris Zervas <dzervas@dzervas.gr>
2026-01-11 15:33:15 +0200

    Fix something?

--- nixos/system/network.nix
+++ nixos/system/network.nix
@@ -20,13 +20,15 @@ in {
     useDHCP = lib.mkDefault true;
     domain = "dzerv.art";
 
+    dhcpcd.denyInterfaces = [ "lo" home-vpn-iface node-vpn-iface "cali*" "podman*" "veth*" ];
+
     firewall = {
       enable = true;
       allowedUDPPorts = [ wireguard-port ]; # WireGuard
 
       extraForwardRules = ''
         # VPN exit node
-        iifname ${node-vpn-iface} oifname eth0 ip saddr ${node-vpn-prefix}.0/24 accept
+        iifname ${home-vpn-iface} oifname eth0 accept
       '';
     };
 
@@ -61,7 +63,7 @@ in {
             inherit name;
 	          inherit (machine) publicKey;
             # NOTE: For some reason I had to manually add the allowed IPs for SOME nodes
-            # wg set wg0 peer 'Owhi+vyqYtFrSs9bOj8qnEsEvOiXD1zME41rLUQ2KV8=' allowed-ips +10.42.101.0/24
+            # wg set wg0 peer <publicKey> allowed-ips +10.42.101.0/24
             allowedIPs = ["${node-vpn-prefix}.${machine.hostIndex}/32" "10.42.${machine.hostIndex}.0/24"];
 
             # Use it as an endpoint only if it's a k3s server
@@ -69,7 +71,7 @@ in {
             # endpoint = if builtins.hasAttr "role" machine && machine.role == "server" then "${name}.${config.networking.domain}:${toString wireguard-port}" else null;
             endpoint = if name != "srv0" then "${name}.${config.networking.domain}:${toString wireguard-port}" else null;
             # persistentKeepalive = if builtins.hasAttr "role" machine && machine.role == "server" then null else 25;
-            persistentKeepalive = if name != "srv0" then null else 25;
+            persistentKeepalive = if name != "srv0" && hostName != "srv0" then null else 25;
             dynamicEndpointRefreshSeconds = if name != "srv0" then null else 5;
           } else null)
           machines);
