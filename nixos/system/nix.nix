f58d61b4ff9ef905ba393c1363416e1027fbe801
Dimitris Zervas <dzervas@dzervas.gr>
2026-01-21 18:24:59 +0200

    Remove leftover builder thing

--- nixos/system/nix.nix
+++ nixos/system/nix.nix
@@ -1,19 +1,7 @@
-{ config, lib, machines, pkgs, ... }: let
-  builderUser = "builder";
-in {
+_: {
   nixpkgs.config.allowUnfree = true;
 
-  users.groups.builders = {};
-  users.users.${builderUser} = {
-    isNormalUser = true;
-    createHome = false;
-    group = "builders";
-
-    openssh.authorizedKeys.keys = config.users.users.root.openssh.authorizedKeys.keys;
-  };
-
   nix = {
-    package = pkgs.nix;
     extraOptions = ''
       # Garbage collect when free space is less than 32GB
       min-free = ${toString (32 * 1024 * 1024 * 1024)}
@@ -21,20 +9,15 @@ in {
 
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
-      warn-dirty = false;
       max-jobs = "auto";
       cores = 0;  # Use all available cores
       build-cores = 0;
       http-connections = 50; # Parallel downloads
       auto-optimise-store = true;
-      download-buffer-size = 524288000; # 500MB buffer size
-
-      # Keep more derivations in memory
-      keep-derivations = true;
-      keep-outputs = true;
+      download-buffer-size = 512 * 1024 * 1024; # 500MB buffer size
 
       # Only allow wheel users to run nix
-      trusted-users = [ builderUser "root" ];
+      trusted-users = [ "root" ];
 
       # Binary caches
       substituters = [
@@ -58,22 +41,5 @@ in {
       automatic = true;
       dates = ["09:00:00"];
     };
-
-    # Distribute builds
-    distributedBuilds = true;
-    buildMachines = lib.attrsets.mapAttrsToList (name: machine: let
-      system = if builtins.hasAttr "system" machine then machine.system else "x86_64-linux";
-    in {
-      inherit system;
-      systems = [system];
-
-      supportedFeatures = ["benchmark"] ++ (if name == "srv0" then ["big-parallel"] else []);
-
-      hostName = "${name}.${config.networking.domain}";
-
-      maxJobs = 4;
-      protocol = "ssh-ng"; # Better than SSH, if supported by nix
-      sshUser = builderUser;
-    }) machines;
   };
 }
